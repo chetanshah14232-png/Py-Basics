75615ee846cd80198267b8831d361aadb6ebf512
Chetan Shah <chetanshah14232@gmail.com>
2026-04-23 18:33:47 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,12 @@
+#make simple calculator
+
+#take the value from user
+p = float(input("Put Your First Number : "))
+q = float(input("Put Your Another Number : "))
+
+#showing operations
+print("Addition Of The Given Numbers : ",p+q)
+print("Difference Of The Given Numbers : ",p-q)
+print("Multiplication Of The Given Numbers : ",p*q)
+print("Division Of The Given Numbers : ",p/q)
+print("Remainder Of The Given Numbers : ",p%q)
